a4062f5d7c3ace70902716bd97412a515f42131f
Micks-Tech <mickstech060924@gmail.com>
2026-01-05 17:12:10 +0800

    commit

---
+++ main.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    const MessageContainer = document.querySelector('.message-container');
+    const Button = document.querySelector('button');
+    const ExitPage = this.document.querySelector('.exit-page');
+
+    const messageState = localStorage.getItem('messageOpen');
+
+    if (messageState === 'true') {
+        Button.style.display = 'none';
+        MessageContainer.style.display = 'flex';
+    } else {
+        Button.style.display = 'none';
+        MessageContainer.style.display = 'flex';
+    }
+
+    Button.addEventListener('click', () => {
+        Button.style.display = 'none';
+        MessageContainer.style.display = 'flex';
+        localStorage.setItem('messageOpen', 'true');
+    });
+
+    ExitPage.addEventListener('click', () => {
+        Button.style.display = 'flex';
+        MessageContainer.style.display = 'none';
+        localStorage.setItem('messageOpen', 'false');
+    });
+};
